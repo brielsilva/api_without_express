1f891cc598f98d59eb7797dcdc3bd16730dd889c
Gabriel Costa <gabrielcostasilva100@gmail.com>
2022-02-18 22:21:41 -0300

    added query on the req object using WHATWG URL API, doesn't affect the functionality of the controller for now

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,6 +3,5 @@ const users = require('../mocks/users');
 module.exports = {
     listUsers(req,res) {
         res.writeHead(200,'Content-Type','application/json');
-        return res.end(JSON.stringify(users));
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { NOTFOUND } = require('dns');
 const http = require('http');
+const {URL} = require('url');
 
 const routes = require('./routes.js');
 function error404(req,res) {
@@ -8,10 +8,16 @@ function error404(req,res) {
 }
 
 const server = http.createServer((req,res) => {
+    const host = req.headers.host;
+    const protocol = 'http'
+    const parsedUrl = new URL(`${protocol}://${host}${req.url}`);
+    console.log(parsedUrl);
     const route = routes.find((route) => {
-        return route.endpoint === req.url && route.method === req.method;
+        return route.endpoint === parsedUrl.pathname && route.method === req.method;
     })
     if(route) {
+        req.query = Object.fromEntries(parsedUrl.searchParams);
+        console.log(req.query);
         return route.handler(req,res);
     } else {
         return error404(req,res)
